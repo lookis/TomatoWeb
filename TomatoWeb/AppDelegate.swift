c80c961203de5155ac4da8cf43bc1d8e72da4dac
Lookis <lookisliu@gmail.com>
2017-03-31 16:46:27 +0800

    add first launch check

--- TomatoWeb/AppDelegate.swift
+++ TomatoWeb/AppDelegate.swift
@@ -12,8 +12,16 @@ import SystemConfiguration
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let NOT_FIRST_LAUNCH = "NOT_FIRST_LAUNCH"
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        ServiceController().installService()
+        let serviceController = ServiceController()
+        //first launch
+        if(!UserDefaults.standard.bool(forKey: NOT_FIRST_LAUNCH)){
+            UserDefaults.standard.set(true, forKey: NOT_FIRST_LAUNCH)
+            serviceController.uninstallService();
+        }
+        serviceController.installService()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
